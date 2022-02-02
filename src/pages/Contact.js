c254be65bdc11960ec0620c2e844d371237b27ea
Shubham <shubhamsikarwar43@gmail.com>
2022-02-02 21:56:00 +0530

    contact page

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,10 +1,45 @@
 import React from 'react'
 import TypeEffect from './typeEffect'
+import './Contact.css'
+import { GrFacebookOption, GrInstagram, GrLinkedinOption,GrGithub,GrTwitter } from 'react-icons/gr'
 
 function Contact() {
   return (
-    <div>
-      <TypeEffect name='Contact' />
+    <div className='contact'>
+      <div className='contact-heading'>
+        <TypeEffect name='Contact' />
+      </div>
+      <div className='contact-top-contact'>
+        <div className='contact-top-email'>
+          <p className='contact-top-heading'>Email</p>
+          <p className='contact-top-content'>shubhamsikarwar43@gmail.com</p>
+        </div>
+        <div className='contact-top-phone'>
+          <p className='contact-top-heading'>Phone</p>
+          <p className='contact-top-content'>7974706830</p>
+        </div>
+      </div>
+      <div className='contact-social-media'>
+        <div className='contact-social-media-fr'>
+          <div className='social-media-box'>
+            <GrFacebookOption className='social-media-logo' size={35} />
+          </div>
+          <div className='social-media-box-ig'>
+            <GrInstagram className='social-media-logo' size={35} />
+          </div>
+          <div className='social-media-box'>
+            <GrLinkedinOption className='social-media-logo' size={35} />
+          </div>
+        </div>
+        <div className='contact-social-media-fr'>
+          <div className='social-media-box-gh'>
+            <GrGithub className='social-media-logo' size={35} />
+          </div>
+          <div className='social-media-box'>
+            <GrTwitter className='social-media-logo' size={35} />
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
